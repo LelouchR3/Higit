91d46369eb4fcd48271e2982c6dfb028204d693d
LelouchR3 <LelouchR3@outlook.com>
2018-11-21 14:33:38 +0800

    test

--- src/Higit.java
+++ src/Higit.java
@@ -2,5 +2,6 @@ public class Higit {
     public static void main(String arg[]){
         System.out.println("Hi Git");
         System.out.println("Hello Git");
+        System.out.println("你好 Git");
     }
 }
